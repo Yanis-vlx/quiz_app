f399c154004a9445e8619c962c24a070256e9ebf
Yanis Allouache <yanis.allouache175@gmail.com>
2024-06-18 11:35:24 +0200

    Ajouter localConfig.php pour gérer les différences de config locale

--- config/db.php
+++ config/db.php
@@ -3,12 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quiz_app";
+require_once 'localConfig.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname, 3308);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
---
+++ config/localConfig_model.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quiz_app";
+$port = 3308;
